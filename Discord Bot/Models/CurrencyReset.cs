67c953e4cff73e5a82be38e95d16cec81126ccda
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2024-05-25 14:56:59 -0400

    frontend almost complete

---
+++ Discord Bot/Models/CurrencyReset.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class CurrencyReset
+    {
+        public int Id { get; set; }
+        public ulong GuildId { get; set; }
+        public int DaysBetween { get; set; }
+        public bool Auto { get; set; }
+        public int? CurrencyId { get; set; }
+    }
+}

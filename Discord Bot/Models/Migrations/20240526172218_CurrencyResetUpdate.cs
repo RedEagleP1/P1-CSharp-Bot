a036b89dedaef372622727eff5f718383ff27c2b
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2024-05-26 16:03:46 -0400

    Backend Support
    -Can Reset currency on the website
    -Can make the currency reset every certain amount of days
    -Can make the currency reset at the end of the month
    -Debug command that can be easily modified to do anything. Disabled until you remove comments in DiscordBot.cs

---
+++ Discord Bot/Models/Migrations/20240526172218_CurrencyResetUpdate.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Models.Migrations
+{
+    public partial class CurrencyResetUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "DaysLeft",
+                table: "CurrencyResets",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DaysLeft",
+                table: "CurrencyResets");
+        }
+    }
+}

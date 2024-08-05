13ca4118166328e544a93b76379fc02fac891e89
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2024-08-05 14:07:43 -0400

    almost frontend setup

--- Discord Bot/Models/Automations.cs
+++ Discord Bot/Models/Automations.cs
@@ -16,6 +16,7 @@ namespace Models
 	{
 		public int Id { get; set; }
 		public int SelectedOption { get; set; }
+		public int Type { get; set; }
 		public string? Value { get; set; }
 		public int? AutomationId { get; set; }
 	}

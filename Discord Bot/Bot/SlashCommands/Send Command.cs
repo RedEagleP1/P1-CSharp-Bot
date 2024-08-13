36f1209775732c4c72c458634a66c316d7d99227
Rakshaan Khan <55557308+rakshaankhan@users.noreply.github.com>
2024-08-13 19:07:06 -0400

    Created SendCommand

---
+++ Discord Bot/Bot/SlashCommands/Send Command.cs
@@ -0,0 +1,37 @@
+﻿
+using Bot.SlashCommands.ResponseHelpers;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace Bot.SlashCommands
+{
+    internal class SendCommand : ISlashCommand
+    {
+        public string Name => throw new NotImplementedException();
+
+        public SlashCommandProperties Properties => throw new NotImplementedException();
+
+        public Task HandleCommand(SocketSlashCommand command)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+
+
+    }
+
+}
+
+

7c68a830ba829a85db2e3ec3f9709bca2f4db032
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2024-08-14 15:39:31 -0400

    module finished

---
+++ Discord Bot/Models/Migrations/20240814192804_AddShopItems.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Models.Migrations
+{
+    public partial class AddShopItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ShopItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    GuildId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    ItemName = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    emojiId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    CurrencyId = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    Cost = table.Column<int>(type: "int", nullable: false),
+                    ItemEffectID = table.Column<ulong>(type: "bigint unsigned", nullable: false),
+                    ItemEffectVal = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShopItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ShopItems_Guilds_GuildId",
+                        column: x => x.GuildId,
+                        principalTable: "Guilds",
+                        principalColumn: "Id");
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShopItems_GuildId",
+                table: "ShopItems",
+                column: "GuildId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ShopItems");
+        }
+    }
+}

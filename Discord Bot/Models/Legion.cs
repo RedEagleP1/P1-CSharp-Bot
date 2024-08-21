58372c5d5113079748aa35696bfb7d8bbd5ba8eb
Megafont <51346737+Megafont@users.noreply.github.com>
2024-08-21 16:21:22 -0500

    Implemented TeamSettings module
    + Updated org and legion commands to use the values stored in TeamSettings now.
    + Also fixed a bug in orgInfo and legionInfo commands.

--- Discord Bot/Models/Legion.cs
+++ Discord Bot/Models/Legion.cs
@@ -6,15 +6,20 @@ namespace Models
 {
 	public class Legion
 	{
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public const bool ALLOW_ORG_TO_JOIN_MULTIPLE_LEGIONS = false; // I added this option mostly for easy debugging.
+		public const int DEFAULT_MAX_ORGS = 13; // The max number of organizations that a legion can have.
+		public const int DEFAULT_MIN_NAME_LENGTH = 3; // The minimum length that a legion name must be.
+		public const string MODERATOR_ROLE = "Admin"; // This is the role that is allowed to delete legions.
+
+
+
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public ulong Id { get; set; }
 
         [Required]
         public ulong LeaderID { get; set; }
         [Required]
         public string Name { get; set; }
-        [Required]
-        public int MaxMembers { get; set; }
         [Required]
         public ulong GuildId { get; set; }
     }

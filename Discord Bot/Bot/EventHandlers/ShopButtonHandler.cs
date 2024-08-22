fdee7a11833c4605ad7c1d1967619d86c6240271
wolfyman411 <113002152+wolfyman411@users.noreply.github.com>
2024-08-22 17:03:41 -0400

    inventory partially working
    -bugs with shop
    -bugs with indexing

--- Discord Bot/Bot/EventHandlers/ShopButtonHandler.cs
+++ Discord Bot/Bot/EventHandlers/ShopButtonHandler.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Threading.Channels;
 using System.Threading.Tasks;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using Bot.SlashCommands.Shop;
 
 namespace Bot.EventHandlers
 {
@@ -36,8 +37,13 @@ namespace Bot.EventHandlers
 
 				if (parts[0] == "shopBtn")
                 {
-					await UpdateShop(component, parts);
-                }
+                    var buttonBuilder = new ComponentBuilder();
+					var embedBuilder = new EmbedBuilder();
+
+					await ShopManager.UpdateShop(component.GuildId,component.User,parts, embedBuilder, buttonBuilder);
+					await component.RespondAsync(embed: embedBuilder.Build(), components: buttonBuilder.Build());
+					await component.Message.DeleteAsync();
+				}
             }
             catch (Exception ex)
             {
@@ -47,62 +53,6 @@ namespace Bot.EventHandlers
             {
                 DBReadWrite.ReleaseLock();
             }
-        }
-		private async Task UpdateShop(IComponentInteraction component, string[] parts)
-		{
-			try
-			{
-				using var context = DBContextFactory.GetNewContext();
-				var embedBuilder = new EmbedBuilder();
-
-				List<ShopItem> itemReferences = context.ShopItems
-					.Where(x => x.GuildId == component.GuildId)
-					.ToList();
-
-				var index = int.Parse(parts[1]);
-				var CurrencyRef = await context.Currencies.FirstAsync(x => x.Id == itemReferences[index].CurrencyId);
-				var EditedDesc = itemReferences[index].Description;
-
-				if (EditedDesc.Length > 20)
-				{
-					EditedDesc = EditedDesc.Substring(0, 20);
-					EditedDesc += "...";
-				}
-
-				embedBuilder
-					.WithAuthor(component.User.Username, component.User.GetAvatarUrl() ?? component.User.GetDefaultAvatarUrl())
-					.WithTitle($"{itemReferences[index].ItemName} :{itemReferences[index].emojiId}:")
-					.WithDescription($"**ID:** {itemReferences[index].Id} \n " +
-									 $"**Currency Type:** {CurrencyRef.Name} \n " +
-									 $"**Cost:** {itemReferences[index].Cost} \n " +
-									 $"**Description:** {itemReferences[index].Description}")
-					.WithColor(Color.Blue)
-					.WithCurrentTimestamp();
-
-				var backCap = false;
-				var frontCap = false;
-
-				if (index >= itemReferences.Count-1)
-				{
-					frontCap = true;
-				}
-				else if (index == 0)
-				{
-					backCap = true;
-				}
-
-				var buttonBuilder = new ComponentBuilder()
-					.WithButton(customId: $"shopBtn_{index - 1}", emote: new Emoji("⬅"), disabled: backCap)
-					.WithButton(customId: $"shopBtn_{index + 1}", emote: new Emoji("➡️"), disabled: frontCap)
-					.WithButton(customId: $"buyBtn_{itemReferences[index].Id}", emote: new Emoji("🛒"), style: ButtonStyle.Success);
-
-				await component.RespondAsync(embed: embedBuilder.Build(), components:buttonBuilder.Build());
-				await component.Message.DeleteAsync();
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine($"An error occurred: {ex}");
-			}
 		}
 	}
 }
---
+++ Discord Bot/Bot/SlashCommands/Shop/Shop_ItemDisplay.cs
@@ -0,0 +1,118 @@
+﻿using Bot.SlashCommands.ResponseHelpers;
+using Discord;
+using Discord.WebSocket;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bot.SlashCommands.Shop
+{
+	public class ShopManager
+	{
+		public static async Task UpdateShop(ulong? guildId, SocketUser user, string[] parts, EmbedBuilder embedbuilderNew, ComponentBuilder buttonbuilderNew, bool swapItems = true, bool fullDesc = false, bool buyItems = true, bool showInventory = false)
+		{
+			try
+			{
+				using var context = DBContextFactory.GetNewContext();
+
+				List<ShopItem> itemReferences = new List<ShopItem>();
+				if (showInventory)
+				{
+					var tempList = context.ItemInventories
+					.Where(x => x.userId == user.Id && x.guildId == guildId)
+					.ToList();
+
+					foreach (var item in tempList)
+					{
+						ShopItem shopItem = await context.ShopItems.FirstAsync(x => x.Id == item.itemId);
+						itemReferences.Add(shopItem);
+					}
+				}
+				else
+				{
+					itemReferences = context.ShopItems
+					.Where(x => x.GuildId == guildId)
+					.ToList();
+				}
+
+				var index = int.Parse(parts[1]);
+				var CurrencyRef = await context.Currencies.FirstAsync(x => x.Id == itemReferences[index].CurrencyId);
+				var EditedDesc = itemReferences[index].Description;
+
+				if (EditedDesc.Length > 20 && !fullDesc)
+				{
+					EditedDesc = EditedDesc.Substring(0, 20);
+					EditedDesc += "...";
+				}
+
+				embedbuilderNew
+					.WithAuthor(user.Username, user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl())
+					.WithTitle($"{itemReferences[index].ItemName} :{itemReferences[index].emojiId}:")
+					.WithDescription($"**ID:** {itemReferences[index].Id} \n " +
+									 $"**Currency Type:** {CurrencyRef.Name} \n " +
+									 $"**Cost:** {itemReferences[index].Cost} \n " +
+									 $"**Description:** {EditedDesc}")
+					.WithColor(Color.Blue)
+					.WithCurrentTimestamp();
+
+				if (showInventory)
+				{
+					var tempList = context.ItemInventories
+					.Where(x => x.userId == user.Id && x.guildId == guildId)
+					.ToList();
+					embedbuilderNew
+							.WithAuthor(user.Username, user.GetAvatarUrl() ?? user.GetDefaultAvatarUrl())
+						.WithTitle($"{itemReferences[index].ItemName} :{itemReferences[index].emojiId}:")
+						.WithDescription($"**ID:** {itemReferences[index].Id} \n " +
+										 $"**Currency Type:** {CurrencyRef.Name} \n " +
+										 $"**Cost:** {itemReferences[index].Cost} \n " +
+										 $"**Description:** {EditedDesc} \n " +
+										 $"**Amount:** {tempList[index].amount}")
+						.WithColor(Color.Blue)
+						.WithCurrentTimestamp();
+				}
+
+
+				var backCap = false;
+				var frontCap = false;
+
+				if (index >= itemReferences.Count - 1)
+				{
+					frontCap = true;
+				}
+				else if (index == 0)
+				{
+					Console.WriteLine($"{index}");
+					backCap = true;
+				}
+
+				if (swapItems)
+				{
+					buttonbuilderNew
+						.WithButton(customId: $"shopBtn_{index - 1}", emote: new Emoji("⬅"), disabled: backCap)
+						.WithButton(customId: $"shopBtn_{index + 1}", emote: new Emoji("➡️"), disabled: frontCap);
+				}
+
+				if (buyItems && !swapItems)
+				{
+					buttonbuilderNew
+						.WithButton(customId: $"buyBtn_{itemReferences[index].Id}", emote: new Emoji("🛒"), style: ButtonStyle.Success);
+				}
+				else if (buyItems)
+				{
+					buttonbuilderNew
+						.WithButton(customId: $"shopBtn_{index - 1}", emote: new Emoji("⬅"), disabled: backCap)
+						.WithButton(customId: $"shopBtn_{index + 1}", emote: new Emoji("➡️"), disabled: frontCap)
+						.WithButton(customId: $"buyBtn_{itemReferences[index].Id}", emote: new Emoji("🛒"), style: ButtonStyle.Success);
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"An error occurred: {ex}");
+			}
+		}
+	}
+}
